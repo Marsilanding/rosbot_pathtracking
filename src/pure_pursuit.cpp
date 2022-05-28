dca6d2ed107d234eac26cf9afc0c54dc13f0c12b
Marsilanding <andresmarsil77@gmail.com>
2022-05-28 11:52:41 +0200

    corrected pure pursuit node

--- src/pure_pursuit.cpp
+++ src/pure_pursuit.cpp
@@ -14,9 +14,11 @@ nav_msgs::Odometry currentPose;
 float currentX, currentY, currentAlpha;
 float lookAhead = 2;
 float linearSpeed = 0.1;
+float sat = 1;
 int nextWayPoint = 1;
+int goalX, goalY;
 
-float alpha = 0;
+float alphaRef = 0;
 float angleSpeed = 0;
 
 void updatePos(const nav_msgs::Odometry::ConstPtr& msg){
@@ -39,94 +41,146 @@ void updatePos(const nav_msgs::Odometry::ConstPtr& msg){
 
 void computeNextWayPoint(int path[100][2], int pathSize, float lookAhead)
 {
-  float distance;
+  float lookaheadRelativeDistance;
+  lookaheadRelativeDistance = sqrt(pow((currentX - (float)path[nextWayPoint][0]),2) + pow((currentY - (float)path[nextWayPoint][1]),2)) - lookAhead;
 
-  distance = sqrt(pow((currentX - (float)path[nextWayPoint][0]),2) + pow((currentY - (float)path[nextWayPoint][1]),2)) - lookAhead;
-  ROS_INFO("Distance to lookAhead: %f", distance);
-  if (distance<0.0 && nextWayPoint<pathSize)
+  if (lookaheadRelativeDistance<0.0 && nextWayPoint< pathSize-1)
   {
     nextWayPoint++;
-    ROS_INFO("Moving towards (%d,%d)", path[nextWayPoint][0], path[nextWayPoint][1]);
   }
 }
 
-void computeAlpha(int *goal, float lookAhead)
+float computeAlpha(int *goal, float lookAhead, float currentAlpha)
 {
   // Hay que transformar las coordenadas globales del WayPoint a los ejes del robot
 
   // Calcula el vector desde el robot al punto
   float Px, Py, gy;
+  float alphaError;
   Px = goal[0] - currentX;
   Py = goal[1] - currentY;
 
   // Calcula la proyección del vector del punto 
   // sobre el eje Y del robot
 
-  gy = Px*sin(currentAlpha) + Py*cos(currentAlpha);
-  //float alpha = (2*abs(gy)/lookAhead)*angleSpeed;
-  alpha = (2*gy/(lookAhead*lookAhead));
-  angleSpeed = -alpha;
-  //ROS_INFO("Ref = %f \t Err = %f", alpha, err);
+  //gy = Px*sin(currentAlpha) + Py*cos(currentAlpha);
+
+  gy = Py/sin(currentAlpha);
+
+  alphaRef = (2*abs(gy)/(lookAhead*lookAhead));
+  alphaError = alphaRef - currentAlpha;
+  
+  return alphaError;
 }
 
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pure_pursuit");
-  if (argc != 5)
+  if (argc != 1)
   {
-    ROS_INFO("Provide x and y coordinates of goal, look ahead distance (m) and linear speed (m/s)");
-    return 1;
+    ROS_INFO("Error calling tracker");
   }
 
+
   ros::NodeHandle n;
+
+  n.setParam("rosbot_pure_pursuit_tracker/LookAhead", lookAhead);
+  n.setParam("rosbot_pure_pursuit_tracker/LinearVel", linearSpeed);
+
   ros::ServiceClient path_client = n.serviceClient<pathtracking::GetPath>("get_path");
   ros::Publisher cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
   ros::Subscriber odom_sub = n.subscribe("odom", 10, updatePos);
   pathtracking::GetPath srv;
-  srv.request.x = atoll(argv[1]);
-  srv.request.y = atoll(argv[2]);
-  lookAhead = std::stof(argv[3]);
-  linearSpeed = std::stof(argv[4]);
+
+  std::cout << "Type x goal: ";
+  std::cin >> goalX;
+  std::cout << "Type x goal: ";
+  std::cin >> goalY;
+
+  srv.request.x = goalX;
+  srv.request.y = goalY;
+
+  n.getParam("rosbot_pure_pursuit_tracker/LookAhead", lookAhead);
+  n.getParam("rosbot_pure_pursuit_tracker/LinearVel", linearSpeed);
 
 
-  int path_length = 0;
+  int pathSize = 0;
   int path[100][2];
+  
 
   if (path_client.call(srv))
   {
-    ROS_INFO("Path received");
-    path_length = sizeof(srv.response.x); // Path size
-
-    for(int i = 0; i < path_length; i++)
-    {
+    pathSize = sizeof(srv.response.x);
+    for(int i=0; i<100; i++){
       path[i][0] = srv.response.x[i];
       path[i][1] = srv.response.y[i];
-      ROS_INFO("(%d,%d)", path[i][0], path[i][1]);
+      if (path[i][0] == goalX && path[i][1] == goalY){
+        pathSize = i+1;
+        break;
+      }
     }
+    ROS_INFO("Path received with %d waypoints", pathSize);
   }
   else
   {
-    ROS_ERROR("Failed to call service getPath");
+    ROS_ERROR("Failed to call service");
     return 1;
   }
 
   ros::Rate r(SAMPLE_RATE); // 10 hz
+  geometry_msgs::Twist command;
+
+  float angleSpeed = 0.0;
+  float relativeDistance = 0.0;
+  float admissibleDistanceToGoal = 0.25;
+
   while(ros::ok())
   {
-    //ROS_INFO("Length of path = %d", path_length);
-    computeNextWayPoint(path, path_length, lookAhead);
-    computeAlpha(path[nextWayPoint], lookAhead);
+    //ROS_INFO("Length of path = %d", pathSize);
+    computeNextWayPoint(path, pathSize, lookAhead);
+    angleSpeed = computeAlpha(path[nextWayPoint], lookAhead, currentAlpha);
 
-    // Publish the command to change orientation
-    geometry_msgs::Twist command;
+    relativeDistance = sqrt(pow((currentX - (float)path[nextWayPoint][0]),2) + pow((currentY - (float)path[nextWayPoint][1]),2));
+    ROS_INFO("Moving to (%d,%d). Relative distance: %.2f. Yaw error: %.2f", path[nextWayPoint][0], path[nextWayPoint][1], relativeDistance, angleSpeed);
 
-    command.angular.z = angleSpeed;
+
     command.angular.x = 0;
     command.angular.y = 0;
+    command.angular.z = angleSpeed;
     command.linear.x = linearSpeed;
     command.linear.y = 0;
     command.linear.z = 0;
+
+    if(path[nextWayPoint][0] == goalX && path[nextWayPoint][1] == goalY){
+      if (relativeDistance < admissibleDistanceToGoal) {
+        command.angular.z = 0.0;
+        command.linear.x = 0.0;
+        ROS_INFO("Path Completed");
+      }
+    }
+    else{
+      computeNextWayPoint(path, pathSize, lookAhead);
+      ROS_INFO("Moving towards (%d,%d)", path[nextWayPoint][0], path[nextWayPoint][1]);
+      angleSpeed = computeAlpha(path[nextWayPoint], lookAhead, currentAlpha);
+      ROS_INFO("Yaw error: %.2f", angleSpeed);
+
+      command.angular.x = 0;
+      command.angular.y = 0;
+      command.angular.z = angleSpeed;
+      command.linear.x = linearSpeed;
+      command.linear.y = 0;
+      command.linear.z = 0;
+    }
+
+    //saturations
+
+    if(command.angular.z > sat){
+      command.angular.z = sat;
+    }
+    else if (command.angular.z < -sat){
+      command.angular.z = -sat;
+    }
 
     cmd_pub.publish(command);
     ros::spinOnce();
