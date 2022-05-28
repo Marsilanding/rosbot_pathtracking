f6fe8ee786f8d9a6841a6d2d31950abb3b87bbf2
Pedro Macias <pedmacros@gmail.com>
2022-05-28 19:15:10 +0200

    Minor fix of matplotlib on astar

--- src/trayectoria_astar.py
+++ src/trayectoria_astar.py
@@ -9,7 +9,7 @@ from pathtracking.srv import GetPath, GetPathResponse
 import tf.transformations
 import numpy as np
 #import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 
 #fig = plt.figure()
 #ax = fig.add_subplot(111)
